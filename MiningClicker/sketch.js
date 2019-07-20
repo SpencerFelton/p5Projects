4aee9b5d75b75fdac4dc9553fa2c3b6d842c47f7
Spencer Felton <SpencerEFelton@gmail.com>
2019-07-20 20:14:34 +0900

    updating counter

--- MiningClicker/sketch.js
+++ MiningClicker/sketch.js
@@ -4,9 +4,14 @@ function setup(){
   createCanvas(width, height);
   img = loadImage('Images/rock.png');
   img2 = loadImage('Images/tree.png');
+  x = 0
 }
 
 function draw(){
+  background(255, 255, 255)
+  textSize(30)
+  text(x, 30, 30)
+  x+=1
   image(img, width/2, height/2, 100,100);
   image(img2, width/4, height/4, 50,50)
 }
