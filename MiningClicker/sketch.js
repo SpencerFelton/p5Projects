d6be3f78165b595a2b16e9724a071546eb8ff12d
Spencer Felton <SpencerEFelton@gmail.com>
2019-07-20 19:55:31 +0900

    added tree

--- MiningClicker/sketch.js
+++ MiningClicker/sketch.js
@@ -3,8 +3,10 @@ function setup(){
   height = 500
   createCanvas(width, height);
   img = loadImage('Images/rock.png');
+  img2 = loadImage('Images/tree.png');
 }
 
 function draw(){
   image(img, width/2, height/2, 100,100);
+  image(img2, width/4, height/4, 50,50)
 }
