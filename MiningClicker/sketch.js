6cc5b3ad4ddbd500d664d42074825efd4e21adb2
Spencer Felton <SpencerEFelton@gmail.com>
2019-07-20 19:38:02 +0900

    moved image to central screen

--- MiningClicker/sketch.js
+++ MiningClicker/sketch.js
@@ -1,7 +1,10 @@
 function setup(){
+  width = 1000
+  height = 500
+  createCanvas(width, height);
   img = loadImage('Images/rock.png');
 }
 
 function draw(){
-  image(img, 0, 0);
+  image(img, width/2, height/2, 100,100);
 }
